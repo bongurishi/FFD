58c2a73cd7c7c4785536319a7a59f5bdbd44f2ce
Bongu Rishi <bongurishi07@gmail.com>
2025-12-31 22:25:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if 'batch_results' not in st.session_state:
 # ------------------ Load model once ------------------ #
 @st.cache_resource
 def load_alexnet_model():
-  
+   MODEL_PATH = "alexnet_model.pth"
     GDRIVE_ID = "10FwrUt27Z3VWMtMw4rfhql6zX8LCeIfF"
     GDRIVE_URL = f"https://drive.google.com/file/d/1Wq9yuL36YU1WBbqk_7_O6il9kbbdaXY0/view?usp=sharing{GDRIVE_ID}"
 
